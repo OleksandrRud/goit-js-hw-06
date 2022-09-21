bb5f060e3d13654829428263ccf2eb3656f4faff
OleksandrRud <oleksandrrud.ua@gmail.com>
2022-09-21 20:39:29 +0300

    Update task-6

--- js/task-06.js
+++ js/task-06.js
@@ -1,16 +1,19 @@
 const input = document.querySelector("input");
 const textInputArea = document.querySelector("#validation-input");
 
-const valueDataLength = textInputArea.getAttribute("data-length");
+const valueDataLength = textInputArea.getAttribute(["data-length"]);
 
 input.addEventListener("blur", checkCorrectFill);
 
 function checkCorrectFill(e) {
+  console.log(e.currentTarget);
   if (Number(valueDataLength) === e.currentTarget.value.length) {
-    textInputArea.classList.add("valid");
-    textInputArea.classList.remove("invalid");
+    changeClass("valid", "invalid");
   } else {
-    textInputArea.classList.remove("invalid");
-    textInputArea.classList.add("invalid");
+    changeClass("invalid", "valid");
   }
 }
+const changeClass = (addClass, removeClass) => {
+  textInputArea.classList.add(addClass);
+  textInputArea.classList.remove(removeClass);
+};
